776cfdbc78d983c67a0454d2350eadba38568710
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-02-07 13:54:50 -0300

    department: findById implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -8,10 +8,15 @@ public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("Test 1: department insert");
+        /*System.out.println("Test 1: department insert");
         Department newDepartment = new Department(null, "Water");
         departmentDao.insert(newDepartment);
-        System.out.println("Inserted! New id = " + newDepartment.getId());
+        System.out.println("Inserted! New id = " + newDepartment.getId());*/
+
+        System.out.println("Test 2: department findById");
+
+        Department department = departmentDao.findById(7);
+        System.out.println(department);
 
     }
 }
