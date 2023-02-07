9bd1c2b2fda38706006ad72c9db228a2647f4757
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-02-07 10:07:09 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,5 +32,14 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("Test 4: seller insert");
+        Seller newSeller = new Seller(null, "Pedro", "Pedro@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
+        
     }
+
+
 }
